ed45e5c4651e092acea23993bec629dbd87ba413
Pubudu Ishan <pubudu.ishan@laugfs.lk>
2025-04-17 00:52:33 +0530

    Modified on data loader for get data from test data json files

--- Utilities/data_loader.py
+++ Utilities/data_loader.py
@@ -1,6 +1,18 @@
 import json
 import os
 
+def load_titles_from_json(json_path="TestData/test_titles.json"):
+    with open(json_path, "r") as file:
+        return json.load(file)
+
 def load_users_from_json(json_path="TestData/test_users.json"):
+    with open(json_path, "r") as file:
+        return json.load(file)
+
+def load_side_nav_menu_from_json(json_path="TestData/test_side_nav_menu.json"):
+    with open(json_path, "r") as file:
+        return json.load(file)
+
+def load_profile_menu_from_json(json_path="TestData/test_profile_menu.json"):
     with open(json_path, "r") as file:
         return json.load(file)
