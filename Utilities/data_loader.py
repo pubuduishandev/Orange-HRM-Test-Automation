23b3e3c74204856dc9505354d94122c45b5d8da0
Pubudu Ishan <pubudu.ishan@laugfs.lk>
2025-04-18 23:49:45 +0530

    Changed data loader variable names

--- Utilities/data_loader.py
+++ Utilities/data_loader.py
@@ -1,7 +1,6 @@
 import json
-import os
 
-def load_titles_from_json(json_path="TestData/test_titles.json"):
+def load_urls_from_json(json_path="TestData/test_urls.json"):
     with open(json_path, "r") as file:
         return json.load(file)
 
@@ -9,10 +8,10 @@ def load_users_from_json(json_path="TestData/test_users.json"):
     with open(json_path, "r") as file:
         return json.load(file)
 
-def load_side_nav_menu_from_json(json_path="TestData/test_side_nav_menu.json"):
+def load_side_nav_menu_items_from_json(json_path="TestData/test_side_nav_menu_items.json"):
     with open(json_path, "r") as file:
         return json.load(file)
 
-def load_profile_menu_from_json(json_path="TestData/test_profile_menu.json"):
+def load_profile_menu_items_from_json(json_path="TestData/test_profile_menu_items.json"):
     with open(json_path, "r") as file:
         return json.load(file)
